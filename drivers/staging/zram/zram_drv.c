49b4059089f663642ae5d7fdd22b8841a818e04f
Jiang Liu <liuj97@gmail.com>
2013-06-07 00:07:29 +0800

    zram: kill unused zram_get_num_devices()
    Now there's no caller of zram_get_num_devices(), so kill it.
    And change zram_devices to static because it's only used in zram_drv.c.
    
    Signed-off-by: Jiang Liu <jiang.liu@huawei.com>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -55,7 +55,7 @@ module_param_named(lzo_algo_type, lzo_algo_type, uint, S_IRUGO);
 
 /* Globals */
 static int zram_major;
-struct zram *zram_devices;
+static struct zram *zram_devices;
 
 /* Module params (documentation at end) */
 static unsigned int num_devices = 1;
@@ -705,11 +705,6 @@ static void destroy_device(struct zram *zram)
 
 	if (zram->queue)
 		blk_cleanup_queue(zram->queue);
-}
-
-unsigned int zram_get_num_devices(void)
-{
-	return num_devices;
 }
 
 static int __init zram_init(void)
