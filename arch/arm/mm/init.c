941062ad8bd916169f0d914e23d9b89433ab0096
Russell King <rmk+kernel@arm.linux.org.uk>
2015-03-26 11:43:51 +0000

    ARM: switch to use the generic show_mem() implementation
    Switch ARM to use the generic show_mem() implementation, which displays
    the statistics from the mm zone rather than walking the page arrays.
    
    Acked-by: Mel Gorman <mgorman <mgorman@suse.de>
    Tested-by: Gregory Fong <gregory.0xf0@gmail.com>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>
    [Backported to v3.10.108]
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

--- arch/arm/mm/init.c
+++ arch/arm/mm/init.c
@@ -83,60 +83,6 @@ void __init early_init_dt_setup_initrd_arch(unsigned long start, unsigned long e
 }
 #endif /* CONFIG_OF_FLATTREE */
 
-/*
- * This keeps memory configuration data used by a couple memory
- * initialization functions, as well as show_mem() for the skipping
- * of holes in the memory map.  It is populated by arm_add_memory().
- */
-struct meminfo meminfo;
-
-void show_mem(unsigned int filter)
-{
-	int free = 0, total = 0, reserved = 0;
-	int shared = 0, cached = 0, slab = 0, i;
-	struct meminfo * mi = &meminfo;
-
-	printk("Mem-info:\n");
-	show_free_areas(filter);
-
-	if (filter & SHOW_MEM_FILTER_PAGE_COUNT)
-		return;
-
-	for_each_bank (i, mi) {
-		struct membank *bank = &mi->bank[i];
-		unsigned int pfn1, pfn2;
-		struct page *page, *end;
-
-		pfn1 = bank_pfn_start(bank);
-		pfn2 = bank_pfn_end(bank);
-
-		page = pfn_to_page(pfn1);
-		end  = pfn_to_page(pfn2 - 1) + 1;
-
-		do {
-			total++;
-			if (PageReserved(page))
-				reserved++;
-			else if (PageSwapCache(page))
-				cached++;
-			else if (PageSlab(page))
-				slab++;
-			else if (!page_count(page))
-				free++;
-			else
-				shared += page_count(page) - 1;
-			page++;
-		} while (page < end);
-	}
-
-	printk("%d pages of RAM\n", total);
-	printk("%d free pages\n", free);
-	printk("%d reserved pages\n", reserved);
-	printk("%d slab pages\n", slab);
-	printk("%d pages shared\n", shared);
-	printk("%d pages swap cached\n", cached);
-}
-
 static void __init find_limits(unsigned long *min, unsigned long *max_low,
 			       unsigned long *max_high)
 {
