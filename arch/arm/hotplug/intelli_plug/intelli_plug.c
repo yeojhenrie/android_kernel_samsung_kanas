c5825e2eba3db6f8fac9d605bb1f58c94291f505
Mark Enriquez <enriquezmark36@gmail.com>
2019-09-25 13:09:42 +0800

    intelliactive: Debug off and disable by default
    Signed-off-by: Mark Enriquez <enriquezmark36@gmail.com>

--- arch/arm/hotplug/intelli_plug/intelli_plug.c
+++ arch/arm/hotplug/intelli_plug/intelli_plug.c
@@ -79,7 +79,7 @@ struct ip_cpu_info {
 static DEFINE_PER_CPU(struct ip_cpu_info, ip_info);
 
 /* HotPlug Driver controls */
-static atomic_t intelli_plug_active = ATOMIC_INIT(1);
+static atomic_t intelli_plug_active = ATOMIC_INIT(0);
 static unsigned int cpus_boosted = DEFAULT_NR_CPUS_BOOSTED;
 static unsigned int min_cpus_online = DEFAULT_MIN_CPUS_ONLINE;
 static unsigned int max_cpus_online = DEFAULT_MAX_CPUS_ONLINE;
@@ -97,7 +97,7 @@ static u64 boost_lock_duration = BOOST_LOCK_DUR;
 static unsigned int def_sampling_ms = DEF_SAMPLING_MS;
 static unsigned int nr_fshift = DEFAULT_NR_FSHIFT;
 static unsigned int nr_run_hysteresis = DEFAULT_MAX_CPUS_ONLINE * 2;
-static unsigned int debug_intelli_plug = 1;
+static unsigned int debug_intelli_plug = 0;
 
 #define dprintk(msg...)		\
 do { 				\
